d090b0770405ee6f7ca904f0701da64815d2034d
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2017-12-21 12:11:51 +0200

    feat(): initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # webpack-minimal-svgstore-plugin
+
+Todo:
+1. Loader to require files from the source code, not plugin options
+2. Plugins options validation
---
+++ index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const svgstore = require('svgstore');
+
+let ConcatSource;
+
+try {
+    ConcatSource = require('webpack-core/lib/ConcatSource');
+} catch (e) {
+    ConcatSource = require('webpack-sources').ConcatSource;
+}
+
+class MinimalSvgStoreWebpackPlugin {
+    constructor(options) {
+        this.options = options || {};
+        this.chunks = this.options.chunks || {};
+    }
+
+    apply(compiler) {
+        compiler.plugin('emit', (compilation, callback) => {
+            // prepend payload to existing code
+            const payload = this._generatePayload();
+            console.log(payload);
+            compilation.assets[this.options.asset] = new ConcatSource(
+                payload,
+                compilation.assets[this.options.asset]
+            );
+
+            callback();
+        });
+    }
+
+    _generatePayload() {
+        const sprites = this._generateSprites();
+        const payload = `
+(function (document) {
+    var container = document.querySelector('body');
+
+    if (container) {
+        container.insertAdjacentHTML('afterbegin', ${JSON.stringify(sprites)});
+    } else {
+        throw new Error('svginjector: Could not find element: body');
+    }
+})(document);
+        `;
+
+        return payload;
+    }
+
+    _generateSprites() {
+        const sprites = svgstore();
+
+        for (let iconPath of this.options.icons) {
+            const iconContent = fs.readFileSync(iconPath, 'utf8');
+            const iconName = this.options.prefix + path.basename(iconPath).replace(/\.svg$/, '');
+            sprites.add(iconName, iconContent);
+        }
+
+        return sprites.toString();
+    }
+}
+
+module.exports = MinimalSvgStoreWebpackPlugin;
