a242b76af9da211e86ec660247024b6b814ee58c
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2018-02-26 15:42:52 +0200

    fix(): loader name

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ class MinimalSvgStoreWebpackPlugin {
      */
     _getSvgFilePaths(modules) {
         const svgFilePaths = _.flatten(modules
-            .filter(module => /webpack-minimal-svgstore-loader.+!/ig.test(module.request))
+            .filter(module => /minimal-svgstore-loader.+!/ig.test(module.request))
             .map(module => {
                 const fileNames = _.flatten(module.mapChunks(
                     chunk => chunk.files.filter(
